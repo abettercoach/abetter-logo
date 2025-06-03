b7855641b8dadc039178023231a0edee2f9af4a4
abettercoach <iris@abetter.coach>
2025-06-03 00:01:13 -0400

    Quadrants Implementation

--- sketch.js
+++ sketch.js
@@ -1,82 +1,135 @@
-let scrollSpeed = 5;
 
-let pixelBox;
+let mainCell;
 
 function setup() {
+  randomSeed(99); //We will use the built-in system random function later.
+
+  //P5 Prams Setup
   createCanvas(400, 400);
-  randomSeed(99);
-  pixelBox = new PixelBox();
+
+  //Let's setup our main cell
+  let cellWidth = 100;
+  let cellHeight = 100;
+  let cellX = width/2 - cellWidth/2; //Position in the middle of the X-axis
+  let cellY = height - cellHeight/2; //Position on the Y-axis such that the bottom of canvas intersects the middle of the image
+  mainCell = new ExplodingCell(cellX,cellY,cellWidth,cellHeight);
   
-  let glow_color = color(255); // glow green
+
+  noStroke();
+  fill(255);
+
+  let glow_color = color(255); // glow white
   drawingContext.shadowColor = glow_color;
 }
 
 function draw() {
-  background(0);
-  pixelBox.show();
+  background(0); //Clear every frame
+
+  mainCell.draw();
 }
 
+let scrollSpeed = 5;
 function mouseWheel(event){
   let scrollDiff = event.deltaY * scrollSpeed
-  pixelBox.move(0,scrollDiff);
+  mainCell.scroll(0,scrollDiff);
   return false;
 }
 
-/*
-Pseudo-Code for 
+// Constants for use in ExplodingCell
+const minCellWidth = 5;
+const minCellHeight = 5;
+const maxCellWidth = 15;
+const maxCellHeight = 15;
+const maxExplodedDistance = 100;
 
+class ExplodingCell {
 
-*/
+  constructor(x,y,width,height) {
+    
+    // Each cell is made up of a size, original position, and quadrants (an array of 4 subcells)
+    this.size = {
+      width: width,
+      height: height,
+    }
 
-class PixelBox {
-  
-  constructor() {
+    this.ogPosition = { 
+      x: x,
+      y: y
+    }
     
-    this.boxWidth = 100;
-    this.boxHeight = this.boxWidth;
+    this.quadrants = [];
     
-    this.x = width/2 - this.boxWidth/2;
-    this.y = height - this.boxHeight/2;
-    
-    this.cells = [];
-    
-    for (let y = 0; y < this.boxHeight; y++) {
-      for (let x = 0; x < this.boxWidth; x++) {
-        
-        const cellXOriginal = x;
-        const cellYOriginal = y;
-        
-        let xDistance = 0;
-        let yDistance = 0;
-        
-        if ( random(0,100) <= 25 ){
-          /* 25% of the time, move pixel away max width or height distance away from original position */
-          xDistance = 2*random(0,this.boxWidth) - this.boxWidth;
-          yDistance = 2*random(0,this.boxHeight) - this.boxHeight;
-        }
-        
-        this.cells.push([cellXOriginal,cellYOriginal, xDistance, yDistance]);
-      }   
+    // Whether this cell becomes a parent cell with quadrants depends on 3 conditions.
+    // The first two relate to the size of the cell.
+    let isTooBig = this.size.width > maxCellWidth && this.size.height > maxCellHeight; //Bigger than max, always split
+    let isNotTooSmall = this.size.width > minCellWidth && this.size.height > minCellHeight; //Smaller than min, never split
+
+    if (isTooBig || isNotTooSmall) {
+      // The third condition is randomness. Split 75% of cells that meet our first two criteria.
+      if ( random(0,100) <= 75 ){
+        let halfWidth = width/2;
+        let halfHeight = height/2;
+
+        let cellNW = new ExplodingCell(x,y,halfWidth,halfHeight);
+        let cellNE = new ExplodingCell(x+halfWidth,y,halfWidth,halfHeight);
+        let cellSW = new ExplodingCell(x,y+halfHeight,halfWidth,halfHeight);
+        let cellSE = new ExplodingCell(x+halfWidth,y+halfHeight,halfWidth,halfHeight);
+
+        this.quadrants = [cellNW, cellNE, cellSW, cellSE]
+        // Cells that met all criteria now have four quadrants or subcells
+      }
+    } // Otherwise, the quadrants array will remain empty 
+
+    // Each cell will also have an "exploded position" calculated by adding dX and dY to the original position.
+    // For most cells this will be the same as the original position, meaning they won't ever appear exploded when drawn.
+    let dX = 0; 
+    let dY = 0;
+
+    // For 25% of the cells, we move the cell away by a random distance away (no further than a set max)
+    if ( random(0,100) <= 50 ){
+      dX = random(0 - maxExplodedDistance,maxExplodedDistance);
+      dY = random(0 - maxExplodedDistance,maxExplodedDistance);
+    }
+
+    this.exPosition = {
+      x: x + dX,
+      y: y + dY
     }
   }
   
-  move(x,y) {
+  scroll(x,y) {
     this.x += x;
     this.y += y;
   }
   
-  show() {
-    noStroke();
-    fill(255);
+  draw() {
     
-    let maxDistanceToCenter = height/2 - this.boxHeight/2;
-    let distanceToCenter = height/2 - (this.y + this.boxHeight/2);
+    // Let's iterate over all cells and their quadrants to render them in their original position.
+    // TODO: Render in exploded position.
+
+    let hasQuadrants = this.quadrants.length > 0;
+    if (hasQuadrants) {
+      this.quadrants[0].draw(); //Draw NW Cell
+      this.quadrants[1].draw(); //Draw NE Cell
+      this.quadrants[2].draw(); //Draw SW Cell
+      this.quadrants[3].draw(); //Draw SE Cell
+    } else {
+      rect(this.exPosition.x, this.exPosition.y, this.size.width, this.size.height);
+    }
+
+    //Below is the old rendering code, before using a quadrants algorithm
+    //Keeping for reference until scrolling and glowing are re-implemented.
+
+    /* 
+    let maxDistanceToCenter = height/2 - this.height/2;
+    let distanceToCenter = height/2 - (this.y + this.height/2);
+
     let movement = maxDistanceToCenter + distanceToCenter;
     
     if (distanceToCenter >= 0) {
       drawingContext.shadowBlur = distanceToCenter;
       
-      rect(this.x,this.y,this.boxWidth,this.boxHeight);
+      rect(this.x,this.y,this.width,this.height);
       
     } else {
       drawingContext.shadowBlur = 0;
@@ -101,6 +154,6 @@ class PixelBox {
         rect(this.x + cellX, this.y + cellY,1,1);
       }
     }
-    
+    */
   }
 }
